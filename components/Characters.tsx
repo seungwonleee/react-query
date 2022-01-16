6f774a97e2e2387d8a9b056482b64605253e1341
seungwonleee <seungwon.code@gmail.com>
2022-01-16 22:25:56 +0900

    fetch data by page

--- components/Characters.tsx
+++ components/Characters.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useQuery } from 'react-query';
 import Card from './Card';
 
 export default function Characters() {
-  const fetchCharacters = async () => {
-    const { data } = await axios(`https://rickandmortyapi.com/api/character`);
-    console.log(data);
+  const [page, setPage] = useState(1);
+
+  const fetchCharacters = async ({
+    queryKey,
+  }: {
+    queryKey: Array<string | number>;
+  }) => {
+    console.log(queryKey);
+    const { data } = await axios(
+      `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+    );
+    // console.log(data);
     return data;
   };
 
-  const { status, data, error } = useQuery('characters', fetchCharacters);
+  const { status, data, isPreviousData, isLoading, isError } = useQuery(
+    ['characters', page],
+    fetchCharacters,
+    { keepPreviousData: true }
+  );
 
-  if (status === 'loading') {
+  if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  if (status === 'error') {
+  if (isError) {
     return <div>Error</div>;
   }
 
@@ -25,6 +38,20 @@ export default function Characters() {
       {data.results.map((character: any) => (
         <Card key={character.name} character={character} />
       ))}
+      <div>
+        <button
+          disabled={page === 1}
+          onClick={() => setPage((prev) => prev - 1)}
+        >
+          이전
+        </button>
+        <button
+          disabled={isPreviousData && !data.info.next}
+          onClick={() => setPage((prev) => prev + 1)}
+        >
+          다음
+        </button>
+      </div>
     </div>
   );
 }
